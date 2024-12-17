fd2685472570b42a8a7dd551ea8e4522f69dc5b4
bengriffiths95 <bengriffiths95@gmail.com>
2024-12-17 15:44:43 +0000

    add github actions workflow file with updated requirements

--- tests/test_retrieve_api_data.py
+++ tests/test_retrieve_api_data.py
@@ -9,12 +9,15 @@ def test_successful_request():
         "https://fantasy.premierleague.com/api/fixtures/",
         json={"key": "value"},
     )
-    output = retrieve_data('fixtures/')
+    output = retrieve_data("fixtures/")
     assert output == {"key": "value"}
+
 
 @responses.activate
 def test_failed_request():
-    responses.get("https://fantasy.premierleague.com/api/test/", body=Exception("404 Not Found"))
+    responses.get(
+        "https://fantasy.premierleague.com/api/test/", body=Exception("404 Not Found")
+    )
     with pytest.raises(Exception) as err:
-        retrieve_data('test/')
-    assert str(err.value) == '404 Not Found'
+        retrieve_data("test/")
+    assert str(err.value) == "404 Not Found"
