feefce11d4d181ce649bb694a02f6c0b9a9b3dd4
bengriffiths95 <bengriffiths95@gmail.com>
2025-01-02 16:31:01 +0000

    add transform util function for converting dataframe to Parquet & saving to S3 bucket

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,5 +1,7 @@
 import json
+from datetime import datetime
 import boto3
+import awswrangler as wr
 
 
 def retrieve_s3_json(bucket_name, file_name):
@@ -10,7 +12,46 @@ def retrieve_s3_json(bucket_name, file_name):
             Key=file_name,
         )
         response = json.loads(response["Body"].read().decode("utf-8"))
+        print(response)
         return response
     except Exception as e:
         print("Error: ", e)
         raise
+
+
+def save_df_to_parquet_s3(table_name, table_df, bucket):
+    """
+    Save a dataframe to s3 bucket
+
+    This function retrieves a desired table_name, dataframe, and s3 bucket name, converts the dataframe to parquet file format and uploads this file to the bucket using awswrangler.
+
+    Parameters:
+        table_name (str): The name of the table which you want to use in the s3 key
+        table_df (DataFrame): The DataFrame object which you want to convert to parquet
+        bucket (str): The name of the S3 bucket you want to upload the file to
+
+    Returns:
+        Nothing
+
+    Side Effects:
+        On success - Parquet file added to S3 bucket
+        On failure - Exception printed to console
+
+    Example:
+        >>> convert_dataframe_to_parquet('example_table', dataframe, 'example-bucket')
+        Print - Saved to s3://{bucket-name/timestamp/table-name}.parquet
+    """
+
+    try:
+        # creates current timestamp for s3 file name
+        current_timestamp = datetime.now().strftime("%Y-%m-%d")
+
+        # set the desired location for the parquet file
+        path = f"s3://{bucket}/{current_timestamp}/{table_name}.parquet"
+
+        # convert the DataFrame to parquet and save to path in s3 bucket
+        output = wr.s3.to_parquet(table_df, path)
+
+        print("Added to bucket: ", output)
+    except Exception as e:
+        print(f"Error processing {table_name}: {e}")
--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,10 +1,12 @@
 import os
 import unittest
+from unittest.mock import patch
 import pytest
 from moto import mock_aws
 import boto3
 from botocore.exceptions import ClientError
-from scripts.transform import retrieve_s3_json
+import pandas as pd
+from scripts.transform import retrieve_s3_json, save_df_to_parquet_s3
 
 
 @mock_aws
@@ -46,3 +48,31 @@ class TestJSONtoList(unittest.TestCase):
             str(err.value)
             == "An error occurred (NoSuchKey) when calling the GetObject operation: The specified key does not exist."
         )
+
+test_df = pd.DataFrame([{'code': 2444613, 'event': None, 'finished': False, 'finished_provisional': False, 'id': 144, 'kickoff_time': None, 'minutes': 0, 'provisional_start_time': True, 'started': None, 'team_a': 12, 'team_a_score': None, 'team_h': 8, 'team_h_score': None, 'stats': [], 'team_h_difficulty': 5, 'team_a_difficulty': 3, 'pulse_id': 115970}])
+
+class TestDfToParquetToS3(unittest.TestCase):
+    @patch("scripts.transform.wr.s3.to_parquet")
+    def test_function_uploads_file_to_s3_bucket(self, mock_df_to_parquet):
+        # mock aws wrangler
+        mock_df_to_parquet.return_value = '{"paths": ["s3://test-bucket/2025-01-02 12:52:03/test.parquet"], "partitions_values": []}'
+
+        # invoke function
+        save_df_to_parquet_s3("test_table", test_df, "test_bucket")
+
+        # check mock was called correctly
+        mock_df_to_parquet.assert_called_once()
+
+    @patch("scripts.transform.wr.s3.to_parquet")
+    @patch("builtins.print")
+    def test_function_raises_exception_for_invalid_bucket(
+        self, mock_print, mock_to_parquet
+    ):
+        # mock exception
+        mock_to_parquet.side_effect = Exception("NoSuchBucket")
+
+        # invoke function
+        save_df_to_parquet_s3("test_table", test_df, "test_bucket")
+
+        # assertion
+        mock_print.assert_any_call("Error processing test_table: NoSuchBucket")
