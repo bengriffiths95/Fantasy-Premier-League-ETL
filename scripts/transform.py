cb02bd987c4f1ef0b4a0e857a421533b11bfd145
bengriffiths95 <bengriffiths95@gmail.com>
2025-01-09 17:09:18 +0000

    added functionality in transform functions to remove null values

--- scripts/transform.py
+++ scripts/transform.py
@@ -118,6 +118,10 @@ def transform_dim_players(bucket_name):
             columns={"id": "player_id", "team_code": "team_id"}, inplace=True
         )
 
+        # dim_players_df['first_name'] = dim_players_df['first_name'].astype(str)
+        # dim_players_df['second_name'] = dim_players_df['second_name'].astype(str)
+        # dim_players_df['web_name'] = dim_players_df['web_name'].astype(str)
+
         # return DataFrame
         return dim_players_df[
             ["first_name", "second_name", "web_name", "player_id", "team_id"]
@@ -191,6 +195,9 @@ def transform_dim_fixtures(bucket_name):
         dim_fixtures_df["fixture_time"] = pd.to_datetime(
             dim_fixtures_df["kickoff_time"]
         ).dt.time
+
+        # replace null values
+        dim_fixtures_df = dim_fixtures_df.fillna(0)
 
         # return DataFrame
         return dim_fixtures_df[
