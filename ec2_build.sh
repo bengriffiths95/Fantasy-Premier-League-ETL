419bef3c64ad320360b01a4a4dd6a52962c8cb14
bengriffiths95 <bengriffiths95@gmail.com>
2025-01-30 14:14:21 +0000

    update ec2_build script

--- ec2_build.sh
+++ ec2_build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+set -e
 
 echo "Create venv"
+if [ ! -d "Fantasy-Premier-League-ETL" ]; then
+    echo "Error: Directory Fantasy-Premier-League-ETL does not exist."
+    exit 1
+fi
 cd Fantasy-Premier-League-ETL/
 python3 -m venv venv
 source venv/bin/activate
@@ -17,12 +22,11 @@ export AIRFLOW_HOME=~/Fantasy-Premier-League-ETL/airflow_home
 echo "Initialise Airflow DB, set up Postgres backend" 
 airflow db init
 sudo apt-get install postgresql postgresql-contrib -y
-sudo -i -u postgres
-psql
+sudo -i -u postgres psql <<EOF
 CREATE DATABASE airflow;
 CREATE USER airflow WITH PASSWORD 'airflow';
 GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
-\q
+EOF
 cd airflow/
 sed -i 's#sqlite:////home/ubuntu/Fantasy-Premier-League-ETL/airflow/airflow.db#postgresql+psycopg2://airflow:airflow@localhost/airflow#g' airflow.cfg
 sed -i 's#SequentialExecutor#LocalExecutor#g' airflow.cfg
@@ -34,4 +38,4 @@ airflow users create -u airflow -f airflow -l airflow -r Admin -e airflow@gmail.
 echo "Please enter account password"
 
 echo "Install other dependencies"
-pip install requirements.txt
+pip install -r requirements.txt
